e96bd2391a6cbeaec4ca90703593a7da4e844279
matias <matias.waehner@hotmail.com>
2020-12-14 14:32:55 -0300

    ahora todo esta en una sola app, appUser ya no existe

--- myrss/views.py
+++ myrss/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 def welcome(request):
     if request.user.is_authenticated:
         return redirect('user_home')
     else:
         return render(request, 'myrss/welcome.html')
+
+
+
+
+@login_required()
+def home(request):
+    return render(request, 'appUser/home.html')
+
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "appUser/signup_form.html"
+    success_url = reverse_lazy('user_home')
