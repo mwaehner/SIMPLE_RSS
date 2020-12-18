de41e55297f686374f88674a5ac8382e1a6e607e
matias <matias.waehner@hotmail.com>
2020-12-18 11:30:08 -0300

    definicion de metodo privado para encapsular

--- myrss/views/new_subscription_view.py
+++ myrss/views/new_subscription_view.py
@@ -18,29 +18,29 @@ class NewSubscriptionView(View):
         form = SubscriptionForm(instance=new_subscription, data=self.request.POST)
         if form.is_valid():
             new_subscription = form.save()
-            url = new_subscription.link
-            news_feed = feedparser.parse(url)
-            for i in range(len(news_feed.entries)):
-                newlink = news_feed.entries[i].get('link')
-                newtitle = news_feed.entries[i].get('title')
-                newsummary = news_feed.entries[i].get('summary')
-                newlinks = news_feed.entries[i].get('links')
-                imglink = ""
-                for j in range(len(newlinks)):
-                    if newlinks!= None and len(newlinks)>0 and re.match('image', newlinks[j].get('type')):
-                        imglink = newlinks[j].get('href')
-                (article, was_created) = Article.objects.get_or_create(
-                    link = news_feed.entries[i].get('link'),
-                    title = news_feed.entries[i].get('title'),
-                    summary = news_feed.entries[i].get('summary'),
-                    img_link = imglink
-                )
-                article.subscriptions.add(new_subscription)
+            self.__add_articles_of_subscription(new_subscription)
+            return redirect('user_home')
 
-        else:
-            my_subs = Subscription.objects.subscriptions_for_user(request.user)
-            return render(request, 'user/home.html', {'form': form, 'subs': my_subs}, status=HTTPStatus.BAD_REQUEST)
-        return redirect('user_home')
+        my_subs = Subscription.objects.subscriptions_for_user(request.user)
+        return render(request, 'user/home.html', {'subscription_form': form, 'subscriptions': my_subs}, status=HTTPStatus.BAD_REQUEST)
+
+    def __add_articles_of_subscription(self, new_subscription):
+        url = new_subscription.link
+        news_feed = feedparser.parse(url)
+        for i in range(len(news_feed.entries)):
+            newlinks = news_feed.entries[i].get('links')
+            imglink = ""
+            for j in range(len(newlinks)):
+                if newlinks != None and len(newlinks) > 0 and re.match('image', newlinks[j].get('type')):
+                    imglink = newlinks[j].get('href')
+            (article, was_created) = Article.objects.get_or_create(
+                link=news_feed.entries[i].get('link'),
+                title=news_feed.entries[i].get('title'),
+                summary=news_feed.entries[i].get('summary'),
+                img_link=imglink
+            )
+            article.subscriptions.add(new_subscription)
+
 
 
 
