3f3da91da4cabded4543645169ba4716a8aa7bd7
matias <matias.waehner@hotmail.com>
2020-12-22 09:57:58 -0300

    ahora el usuario es logueado automaticamente luego de registrarse

--- myrss/views/signup_view.py
+++ myrss/views/signup_view.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 
 
@@ -8,3 +11,12 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "myrss/signup_form.html"
     success_url = reverse_lazy('user_home')
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        #return HttpResponseRedirect(self.get_success_url())
+        return HttpResponseRedirect(reverse_lazy('user_home'))
