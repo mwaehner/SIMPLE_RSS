3a4585fef0a29b0773a0c3c58dcf50744f21c9d0
mwaehner <mwaehner@devartis.com>
2020-12-29 12:13:23 -0300

    cambios en la api, y tests para marcar como leido

--- myrss/urls.py
+++ myrss/urls.py
@@ -27,7 +27,7 @@ from myrss.views.new_subscription_view import NewSubscriptionView
 from myrss.views.show_articles_view import ShowArticlesView
 from myrss.views.update_subscription_view import UpdateSubscriptionView
 from myrss.views.delete_subscription_view import DeleteSubscriptionView
-from myrss.views.change_read_status_view import ChangeReadStatusView
+from myrss.views.toggle_read_view import ToggleRead
 
 
 urlpatterns = [
@@ -45,5 +45,5 @@ urlpatterns = [
     url(r'show_articles/(?P<subscription_id>\d+)/$', ShowArticlesView.as_view(), name="show_articles"),
     url(r'update_subscription/(?P<subscription_id>\d+)/$', UpdateSubscriptionView.as_view(), name="update_subscription"),
     url(r'delete_subscription/(?P<subscription_id>\d+)/$', DeleteSubscriptionView.as_view(), name="delete_subscription"),
-    url(r'change_read_status/(?P<subscription_id>\d+)/(?P<article_id>\d+)/$', ChangeReadStatusView.as_view(), name="change_read_status"),
+    url(r'toggle_read/(?P<article_id>\d+)/$', ToggleRead.as_view(), name="toggle_read"),
 ]
