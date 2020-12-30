95a3dbde4609486faadba60e67a5a71dfa11d3f4
mwaehner <mwaehner@devartis.com>
2020-12-30 13:14:55 -0300

    refactorings en el javascript

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,3 @@
-$('form').submit(function() {
-  $(this).find("button[type='submit']").prop('disabled',true);
-});
-
-$('#showUpdatedNoModal').modal('show');
-
-
-
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -24,9 +16,10 @@ function getCookie(name) {
 const csrftoken = getCookie('csrftoken');
 
 
-$(document).ready(function() {
-    // toggling read status of article when clicking button
-    $('.readstatus').on('click', 'button', function () {
+
+var toggle_read_status_on_button_click = {
+    init : function () {
+        $('.readstatus').on('click', 'button', function () {
 
         if($(this).attr('data-read')=="True"){
             $(this).text("marcar como leido")
@@ -47,9 +40,13 @@ $(document).ready(function() {
         })
 
     })
+    }
 
-    //setting article as read when clicking on link
-    $('.article-text').on('click', 'a', function () {
+}
+
+var set_read_status_on_link_click = {
+    init : function () {
+        $('.article-text').on('click', 'a', function () {
 
         var button = $(this).closest('.article-text').find('.toggle-unread')
         button.text("marcar como no leido")
@@ -62,5 +59,15 @@ $(document).ready(function() {
             }
         })
     })
+    }
+}
+
+$(document).ready( function() {
+    toggle_read_status_on_button_click.init()
+    set_read_status_on_link_click.init()
+    $('form').submit(function() {
+        $(this).find("button[type='submit']").prop('disabled',true);
+    });
+    $('#showUpdatedNoModal').modal('show');
 
 });
